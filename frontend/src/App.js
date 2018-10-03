4cd4075da0c025799ae052d18e8a7f3f25ec56a5
davidw1339 <davidw1339@gmail.com>
2018-10-03 03:51:10 -0500

    Edited homepage header and link to contribution guide

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,16 +8,14 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>Welcome to PhilaREADS homepage!</p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://github.com/hack4impact-uiuc/philareads/blob/master/contributing.md"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Contribute!
           </a>
         </header>
       </div>
