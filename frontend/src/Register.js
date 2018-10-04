b9a6e578f29585fa038689bb31b98c146a676d5c
Josh Byster <joshb1050@gmail.com>
2018-10-03 22:54:17 -0500

    #4 Added initial form inputs

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -7,17 +7,19 @@ class Register extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>Sign up with us below!</p>
+          <form action="/home">
+            <p>Username:</p>
+            <input type="text" />
+            <p>Email:</p>
+            <input type="email" />
+            <p>Password:</p>
+            <input type="password" />
+            <p>Confirm Password:</p>
+            <input type="password" />
+            <br />
+            <input type="submit" />
+          </form>
         </header>
       </div>
     );
