da4761a5a6363650c7bd7faf01aa4fde56b0d871
Michael Leon <mal7@illinois.edu>
2018-10-15 20:40:43 -0500

    Adding in basic test

--- backend/tests/test_basic.py
+++ backend/tests/test_basic.py
@@ -2,9 +2,9 @@
 
 # client passed from client - look into pytest for more info about fixtures
 # test client api: http://flask.pocoo.org/docs/1.0/api/#test-client
-# def test_index(client):
-#     rs = client.get("/")
-#     assert rs.status_code == 200
+def test_index(client):
+    rs = client.get("/")
+    assert rs.status_code == 200
 #
 #
 # def test_get_person(client):
