a3a87d24844ed254bcc9bda814383bbe0861af23
Josh Byster <joshb1050@gmail.com>
2018-10-15 21:31:02 -0500

    Bootstrap-ified login page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,25 +1,39 @@
-import logo from '../logo.svg';
 import React, { Component } from 'react';
+import {
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Button,
+  FormFeedback,
+  FormText
+} from 'reactstrap';
 
-class Login extends Component {
+class Register extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Log in below!</p>
-          <form action="/home">
-            <p>Username:</p>
-            <input type="text" />
-            <p>Password:</p>
-            <input type="password" />
-            <br />
-            <input type="submit" />
-          </form>
-        </header>
+      <div className="container">
+        <h1>Login!</h1>
+        <div className="row">
+          <div className="col-lg-6">
+            <Form method="POST">
+              <FormGroup>
+                <Label for="username">Username</Label>
+                <Input name="username" />
+              </FormGroup>
+              <FormGroup>
+                <Label for="password">Password</Label>
+                <Input name="password" type="password" />
+              </FormGroup>
+              <FormGroup>
+                <Button type="submit">Submit</Button>
+              </FormGroup>
+            </Form>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default Login;
+export default Register;
