3a8c1c4b6f57b4323532f4519d83803c48630b84
Melody Felix <harmonyfelix@gmail.com>
2018-10-22 21:01:27 -0500

    outline of quiz models

--- backend/api/models/Quiz.py
+++ backend/api/models/Quiz.py
@@ -12,7 +12,7 @@ class Quiz(Mixin, db.Model):
     id = db.Column(db.Integer, unique=True, primary_key=True)
     name = db.Column(db.String, nullable=False)
     questions = db.relationship("Question", backref="quiz", lazy=True)
-    book_id = db.Column(db.Integer, db.ForeignKey("quiz.id"), nullable=False)
+    book_id = db.Column(db.Integer, db.ForeignKey("book.id"), nullable=False)
 
     def __init__(self):
         pass
