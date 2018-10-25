dd728cfea32691b188bacd6825a7f9ea4913c034
Alice Fang <alicesf2@illinois.edu>
2018-10-24 23:10:54 -0500

    separated search into filter, results, and search bar components

--- frontend/src/pages/Search.js
+++ frontend/src/pages/Search.js
@@ -1,67 +1,22 @@
 import React, { Component } from 'react';
-import {
-  Button,
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  FormText,
-  Container,
-  Row,
-  Col,
-  Card,
-  CardText,
-  CardBody,
-  CardTitle,
-  CardSubtitle
-} from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
+import SearchBar from '../components/SearchBar';
+import Filter from '../components/Filter';
+import SearchResults from '../components/SearchResults';
 
 class Search extends Component {
   render() {
     return (
       <Container fluid={true}>
         <Row>
-          <Form inline>
-            <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-              <Label for="searchBar" className="mr-sm-2">
-                Search
-              </Label>
-              <Input
-                type="search"
-                name="search"
-                id="searchBar"
-                placeholder="Search for a book..."
-                bsSize="lg"
-              />
-            </FormGroup>
-          </Form>
+          <SearchBar />
         </Row>
-
         <Row>
           <Col lg="4">
-            <Card>
-              <CardBody>
-                <CardTitle>Filter</CardTitle>
-                <CardText>List of filters you can apply</CardText>
-              </CardBody>
-            </Card>
+            <Filter />
           </Col>
-
           <Col lg="8">
-            <Card>
-              <CardBody>
-                <CardTitle>Search Results</CardTitle>
-                <CardText>List of books outputted from search</CardText>
-                <Card>
-                  <CardTitle>Winnie the Pooh 1</CardTitle>
-                  <CardText>Author, Grade, Year</CardText>
-                </Card>
-                <Card>
-                  <CardTitle>Winnie the Pooh 2</CardTitle>
-                  <CardText>Author, Grade, Year</CardText>
-                </Card>
-              </CardBody>
-            </Card>
+            <SearchResults />
           </Col>
         </Row>
       </Container>
