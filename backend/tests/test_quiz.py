45620fecc68c1b6b39473d552b5f97ebfdd1672e
Michael Leon <mal7@illinois.edu>
2018-10-24 22:37:21 -0500

    Fixed formatting

--- backend/tests/test_quiz.py
+++ backend/tests/test_quiz.py
@@ -2,8 +2,10 @@ from api.models import db, User, Quiz
 import pytest
 from flask import current_app, json
 
+
 def setup():
     current_app.config["SECRET_KEY"] = "secret_key"
+
 
 sample_good_json = """
 {
@@ -44,24 +46,24 @@ sample_bad_json_data = """
     ]
 } """
 
+
 def test_create_valid_quiz(client):
     res = client.post(
-        "/create_quiz",
-        data=sample_good_json,
-        content_type="application/json",
+        "/create_quiz", data=sample_good_json, content_type="application/json"
     )
 
     db_quiz = Quiz.query.filter_by(name="Gatsby Quiz")
     assert not (db_quiz is None)
     assert res.status_code == 200
 
+
 def test_create_invalid_quiz(client):
     begin_num_quizzes = len(Quiz.query.all())
     res = client.post(
-        "/create_quiz",
-        data=sample_bad_json_data,
-        content_type="application/json",
+        "/create_quiz", data=sample_bad_json_data, content_type="application/json"
     )
 
-    assert len(Quiz.query.all()) == begin_num_quizzes #no data should have been created
+    assert (
+        len(Quiz.query.all()) == begin_num_quizzes
+    )  # no data should have been created
     assert res.status_code == 422
