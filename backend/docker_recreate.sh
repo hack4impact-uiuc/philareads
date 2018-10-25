ef298e92c4ecbeef7b6221fe072c211d4a632410
David Wang <davidw1339@gmail.com>
2018-10-24 21:58:06 -0500

    added shell script (#56)

---
+++ backend/docker_recreate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+IFS=' '
+docker_ps_out=$(docker ps | grep 'postgres')
+read -ra docker_args <<< "$docker_ps_out"
+docker_id=${docker_args[0]}
+docker stop $docker_id
+echo y | docker system prune
+echo y | docker volume prune
+docker run -e POSTGRES_USER=testusr -e POSTGRES_PASSWORD=password -e POSTGRES_DB=testdb -p 5432:5432 -v flask-app-db:/var/lib/postgresql/data -d postgres:10
