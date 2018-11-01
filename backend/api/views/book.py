b5d34ec080489f031f0afa1918b0e2108efc384e
Melody Felix <harmonyfelix@gmail.com>
2018-10-31 22:30:33 -0500

    api/views/book.py

--- backend/api/views/book.py
+++ backend/api/views/book.py
@@ -5,16 +5,33 @@ from api.core import create_response, serialize_list, logger
 
 book = Blueprint("book", __name__)
 
-@book.route("/add_book", methods=["POST"])
+@book.route("/book", methods=["POST"])
 def create_book():
     print("CREATE BOOK")
     user_data = request.get_json()
 
+    #check all fields are entered
     if('name' not in user_data or 'author' not in user_data):
         return create_response(
             message="Missing name field and/or author field", status=400, data={"status": "failure"}
         )
 
+    #check book if not already in database
+    matching_books = Book.query.filter_by(name=user_data["name"]).all()
+    for book in matching_books:
+        print("BOOK")
+        print(book)
+        print("AUTHOR")
+        print(book.author)
+        if book.author == user_data["author"]:
+            return create_response(
+                message="Duplicate book", status=400, data={"status": "failure"}
+            )
+
+    print("MATCHING_QUIZ")
+    print(matching_books)
+
+    # add book to database
     book =  Book(user_data["name"], user_data["author"])
     db.session.add(book)
     db.session.commit()
@@ -27,6 +44,10 @@ def create_book():
 @book.route("/<book_id>/quizzes", methods=["GET"])
 def get_quizzes(book_id):
     book = Book.query.filter_by(id=book_id).first()
+    print("BOOK")
+    print(book)
+
+    #check to see if book is valid
     if(book is None):
         return create_response(
             message="Book not found", status=400, data={"status": "failure"}
@@ -36,10 +57,13 @@ def get_quizzes(book_id):
 
     print("GETTING QUIZZES")
 
+    # add all quizzes associated with book
     for quiz in book.quizzes:
+        print(quiz)
         quizList.append(quiz.to_dict())
 
     print(quizList)
+
     jsonStr = json.dumps(quizList)
     print(jsonStr)
 
