58b5b657b4229db58c572665cab6eaef189d83f6
Xuan Li Leong <xleong2@illinois.edu>
2018-10-31 23:37:51 -0500

    Add ReadingOlympics routes to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import RegisterPage from './pages/RegisterPage';
-import LoginPage from './pages/LoginPage';
-import './styles/App.scss';
-import KidsPage from './pages/KidsPage';
-import ParentsPage from './pages/ParentsPage';
-import TestPage from './pages/TestPage';
-import Logout from './components/Logout';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import "./styles/App.scss";
+import KidsPage from "./pages/KidsPage";
+import ParentsPage from "./pages/ParentsPage";
+import TestPage from "./pages/TestPage";
+import ROHome from "./pages/ReadingOlympics/ROHome";
+import ROYearGradeView from "./pages/ReadingOlympics/ROYearGradeView";
+import ROYearView from "./pages/ReadingOlympics/ROYearView";
+
+import Logout from "./components/Logout";
 class App extends Component {
   render() {
     return (
@@ -20,6 +24,13 @@ class App extends Component {
           <Route path="/parents" component={ParentsPage} />
           <Route path="/questions" component={TestPage} />
           <Route path="/logout" component={Logout} />
+          <Route exact path="/ReadingOlympics" component={ROHome} />
+          <Route
+            exact
+            path="/ReadingOlympics/:year/:grade(middle|intermediate)"
+            component={ROYearGradeView}
+          />
+          <Route path="/ReadingOlympics/year/:year" component={ROYearView} />
         </div>
       </Router>
     );
