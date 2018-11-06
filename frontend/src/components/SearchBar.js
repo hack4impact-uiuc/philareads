cbb1f26ffe7ca948700c3e98f8863389abd49a61
Alice Fang <32147571+alicesf2@users.noreply.github.com>
2018-11-05 22:56:30 -0600

    RFR: Add graphics to search page (#72)
    * added Search.js page
    
    * finished basic layout of search page
    
    * separated search into filter, results, and search bar components
    
    * separated search into results, filter, search bar component
    
    * added state and prop stuff
    
    * finished search component
    
    * added presearch, loading, and not found states and graphics
    
    * added more styling
    
    * added changing state callbacks to render correct graphic at each state
    
    * return if string query is null

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { Form, FormGroup, Label, Input } from 'reactstrap';
 import PropTypes from 'prop-types';
 import { search } from '../utils/api';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faSearch);
 
 class SearchBar extends Component {
   constructor(props) {
@@ -12,25 +17,19 @@ class SearchBar extends Component {
   }
 
   handleSearch = async () => {
-    // dummy data for search Results
-    // this.props.searchCallback([
-    //   {
-    //     name: "asl",
-    //     author: "asl",
-    //     grade: 1,
-    //     year: 1
-    //   },
-    //   {
-    //     name: "asl",
-    //     author: "asl",
-    //     grade: 1,
-    //     year: 1
-    //   }
-    // ]);
+    if (this.state.query.trim() === '') {
+      return;
+    }
+    this.props.loadCallback();
+
     const { success, result, message } = await search(this.state.query);
 
     if (success) {
-      this.props.searchCallback(result);
+      if (result === []) {
+        this.props.notFoundCallback();
+      } else {
+        this.props.searchCallback(result);
+      }
     } else {
       console.log(message);
     }
@@ -38,10 +37,14 @@ class SearchBar extends Component {
 
   render() {
     return (
-      <Form inline>
+      <Form inline className="searchbar">
         <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-          <Label for="searchBar" className="mr-sm-2">
-            Search
+          <Label for="searchBar">
+            <FontAwesomeIcon
+              className="sm-search-icon"
+              icon="search"
+              size="1x"
+            />
           </Label>
           <Input
             type="text"
@@ -65,6 +68,9 @@ class SearchBar extends Component {
 }
 
 SearchBar.propTypes = {
+  preSearchCallback: PropTypes.func.isRequired,
+  loadCallback: PropTypes.func.isRequired,
+  notFoundCallback: PropTypes.func.isRequired,
   searchCallback: PropTypes.func.isRequired
 };
 
