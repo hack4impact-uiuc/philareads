039458caa48c7c67b3260b7ff6b9af18fe036662
Xuan Li Leong <xleong2@illinois.edu>
2018-11-05 23:27:26 -0600

    Use FA icon for default book

--- frontend/src/components/CatalogCardBook.js
+++ frontend/src/components/CatalogCardBook.js
@@ -9,11 +9,28 @@ import {
 } from 'reactstrap';
 import PropTypes from 'prop-types';
 import { URLParamToString } from '../utils/formatHelpers';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBook } from '@fortawesome/free-solid-svg-icons';
+import '../styles/ReadingOlympics.scss';
 
-const defaultImageSrc =
-  'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b3/Book_font_awesome.svg/2000px-Book_font_awesome.svg.png';
+library.add(faBook);
 
 class CatalogCardBook extends Component {
+  renderBookImage = () => {
+    const { onClickBook } = this.props;
+    const { imageSrc } = this.props.book;
+    if (imageSrc === undefined || imageSrc === null || imageSrc === '') {
+      return (
+        <div className="book-image">
+          <FontAwesomeIcon className="book-icon" icon="book" size="8x" />
+        </div>
+      );
+    }
+
+    return <CardImg src={imageSrc} onClick={onClickBook} alt="" />;
+  };
+
   render() {
     // TODO Should be able to click year/grade to go to that page? Or is that too confusing?
     const { book, onClickBook } = this.props;
@@ -21,11 +38,7 @@ class CatalogCardBook extends Component {
     return (
       <Card>
         <CardBody>
-          <CardImg
-            src={book.imageSrc || defaultImageSrc}
-            onClick={onClickBook}
-            alt=""
-          />
+          {this.renderBookImage()}
           <CardTitle onClick={onClickBook}>
             {`${book.name} by ${book.author}`}
           </CardTitle>
