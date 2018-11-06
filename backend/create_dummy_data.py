a7c54fe6673e20354515c5e64e76e3e04f9a17b3
davidw1339 <davidw1339@gmail.com>
2018-11-06 01:30:07 -0600

    added dummy data script and use a shell script to execute it from scratch

--- backend/create_dummy_data.py
+++ backend/create_dummy_data.py
@@ -1,2 +1,58 @@
-from api import create_app
-from api.models import db, 
+import requests
+
+API_URL = "http://localhost:5000/"
+
+# sample user
+sample_user = {
+    "email": "davidchang@chegg.com",
+    "name": "David Chang",
+    "password": "iwearcrocseveryday"
+}
+
+# create a user
+r = requests.post(API_URL + "register", json=sample_user)
+result = r.json()['result']
+print(result)
+user_token = result['token']
+
+# sample book
+sample_book = {
+    "name": "The Adventures of Huckleberry Finn",
+    "author": "Mark Twain",
+    "grade": 8,
+    "year": 2018,
+    "cover_url": "https://cdn3.volusion.com/jtoq7.b7owf/v/vspfiles/photos/ADVENTURES_OF_HUCKLEBERRY_FINN-2.jpg",
+    "reader_url": "google.com"
+}
+r = requests.post(API_URL + "book", json=sample_book)
+print(r.json())
+
+# sample quizzes
+sample_quiz = {
+    "name": "Huck Finn Quiz 1",
+    "book_id": 1,
+    "questions": [
+        {
+            "text": "Who wrote Huckleberry Finn?",
+            "options": [
+                "Mark Twain",
+                "JK Rowling",
+                "Steinbeck",
+                "Tony Morrison"
+            ],
+            "correct_option": "Mark Twain"
+        },
+        {
+            "text": "Who is Huckleberry Finn's Best Friend?",
+            "options": [
+                "Tom Sawyer",
+                "His dad",
+                "Jake Wiggins",
+                "His dog"
+            ],
+            "correct_option": "Tom Sawyer"
+        }
+    ]
+}
+r = requests.post(API_URL + "quiz", json=sample_quiz)
+print(r.json())
