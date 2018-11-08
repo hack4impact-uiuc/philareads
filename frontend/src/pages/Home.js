d2040544267e51a3451031bc5e7cff30c5e71fb0
hungryflamingcat <hchi124@gmail.com>
2018-11-08 03:48:39 -0600

    RFR: Build Homepage Layout (#81)
    * init commit; started home layout
    
    * styling
    
    * visual, but rough, homepage done
    
    * set up basic routing
    
    * using local images instead of pulling from web
    
    * removed comments and changed <a> tags to <Link>s

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,14 +1,77 @@
 import React, { Component } from 'react';
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  CardTitle,
+  CardImg,
+  CardText
+} from 'reactstrap';
+import { Link } from 'react-router-dom';
+import intermediate from './../images/home_intermediate.png';
+import parents from './../images/home_parents.png';
+import exercises from './../images/home_exercises.png';
+import search from './../images/home_search.png';
+import './../styles/Home.scss';
 
 class Home extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <a href="/register">Register</a>
-          <a href="/login">Login</a>
-          <a href="/kids">Kids</a>
-        </header>
+      <div>
+        <Container>
+          <Row>
+            <Col id="left">
+              <Card className="parent">
+                <h1>Reading Olympics</h1>
+                <Card>
+                  <CardTitle>Middle</CardTitle>
+                  <CardText>Grades 1-3</CardText>
+                  <Link to="/ReadingOlympics/2019/middle">
+                    <span />
+                  </Link>
+                </Card>
+                <Card>
+                  <CardTitle>Intermediate</CardTitle>
+                  <CardText>Grades 4-6</CardText>
+                  <CardImg className="image" src={intermediate} />
+                  <Link to="/ReadingOlympics/2019/intermediate">
+                    <span />
+                  </Link>
+                </Card>
+              </Card>
+            </Col>
+            <Col id="right">
+              <Card>
+                <CardTitle>For Parents</CardTitle>
+                <CardText>
+                  Click here for some advice on how to make the most out of your
+                  child's leaning experience with the Reading Olympics.
+                </CardText>
+                <CardImg className="image" src={parents} />
+                <Link to="/parents">
+                  <span />
+                </Link>
+              </Card>
+              <Card>
+                <CardTitle>Exercising/Archives</CardTitle>
+                <CardText>Start your reading exercises here!</CardText>
+                <CardImg className="image" src={exercises} />
+                <Link to="/#">
+                  <span />
+                </Link>
+              </Card>
+              <Card>
+                <CardTitle>Search</CardTitle>
+                <CardText>Search for your books here!</CardText>
+                <CardImg className="image" src={search} />
+                <Link to="/search">
+                  <span />
+                </Link>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
