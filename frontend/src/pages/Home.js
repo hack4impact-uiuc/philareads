68ae39e67c873da08094dcc8d54711037b3289b7
Helena Chi <hchi124@gmail.com>
2018-11-07 22:55:13 -0600

    a little more colorful and visually appealing but not so much yet

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -15,7 +15,71 @@ import './../styles/Home.scss';
 class Home extends Component {
   render() {
     return (
-      <Container>
+      <div>
+        <Container>
+          <Row>
+            <Col id="left">
+              <Card>
+                <CardTitle>Reading Olympics</CardTitle>
+                <CardSubtitle>Card subtitle</CardSubtitle>
+                <CardText>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </CardText>
+                <Button className="button">Button</Button>
+              </Card>
+            </Col>
+            <Col id="right">
+              <Card>
+                <CardTitle>For Parents</CardTitle>
+                <CardSubtitle>Card subtitle</CardSubtitle>
+                <CardText>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </CardText>
+                <Button className="button">Button</Button>
+              </Card>
+              <Card>
+                <CardTitle>Exercising/Archives</CardTitle>
+                <CardSubtitle>Card subtitle</CardSubtitle>
+                <CardText>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </CardText>
+                <Button className="button">Button</Button>
+              </Card>
+              <Card>
+                <CardTitle>Search</CardTitle>
+                <CardSubtitle>Card subtitle</CardSubtitle>
+                <CardText>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </CardText>
+                <Button className="button">Button</Button>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default Home;
+
+/**
+ * old render return()
+ * <div className="App">
+        <header className="App-header">
+          <a href="/register">Register</a>
+          <a href="/login">Login</a>
+          <a href="/kids">Kids</a>
+        </header>
+      </div>
+ */
+
+/**
+  * <Container>
         <Row>
           <Col>
             <Card>
@@ -59,19 +123,4 @@ class Home extends Component {
           </Col>
         </Row>
       </Container>
-    );
-  }
-}
-
-export default Home;
-
-/**
- * old render return()
- * <div className="App">
-        <header className="App-header">
-          <a href="/register">Register</a>
-          <a href="/login">Login</a>
-          <a href="/kids">Kids</a>
-        </header>
-      </div>
- */
+  */
