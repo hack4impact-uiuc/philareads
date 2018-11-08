8dded8a482266e69c1edae9b43131a8a6d6f05d4
Xuan Li Leong <xleong2+h4i@illinois.edu>
2018-11-08 00:51:03 -0600

    RFR: Set up all the routing (#83)
    * Remove test page
    
    * Remove importing testpage
    
    * Flesh out all the routes
    
    * Properly rebase lol

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,36 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import Home from './pages/Home';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
-import './styles/App.scss';
-import KidsPage from './pages/KidsPage';
+import Logout from './components/Logout';
 import ParentsPage from './pages/ParentsPage';
-import TestPage from './pages/TestPage';
+import Search from './pages/Search';
+import ProfilePage from './pages/ProfilePage';
+import AdminPage from './pages/AdminPage';
 import ROHome from './pages/ReadingOlympics/ROHome';
 import ROYearGradeView from './pages/ReadingOlympics/ROYearGradeView';
 import ROYearView from './pages/ReadingOlympics/ROYearView';
 
-import Logout from './components/Logout';
+import './styles/App.scss';
+
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
           <Route exact path="/" component={Home} />
-          <Route exact path="/register" component={RegisterPage} />
-          <Route exact path="/login" component={LoginPage} />
-          <Route path="/kids" component={KidsPage} />
-          <Route path="/parents" component={ParentsPage} />
-          <Route path="/questions" component={TestPage} />
+          {/*Authentication pages*/}
+          <Route path="/register" component={RegisterPage} />
+          <Route path="/login" component={LoginPage} />
           <Route path="/logout" component={Logout} />
+          {/*Ungrouped pages*/}
+          <Route path="/parents" component={ParentsPage} />
+          <Route path="/search" component={Search} />
+          <Route path="/profile" component={ProfilePage} />
+          <Route path="/admin" component={AdminPage} />
+          {/*Reading Olympics pages*/}
           <Route exact path="/ReadingOlympics" component={ROHome} />
           <Route
             exact
