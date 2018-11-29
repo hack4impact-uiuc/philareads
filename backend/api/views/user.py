8169cac6747b3050f8acf3ad34b99365e4cf40d8
David Wang <davidw1339@gmail.com>
2018-11-28 23:08:40 -0600

    import user hot fixxx (#171)

--- backend/api/views/user.py
+++ backend/api/views/user.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request, Blueprint
 from api.models import User, db
 from api.core import create_response, serialize_list, logger
 import bcrypt
+import jwt
 
 user = Blueprint("user", __name__)
 
