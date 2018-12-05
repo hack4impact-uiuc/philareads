ec508953c2cf9a855f64ea1d00834ecc66206e51
Xuan Li Leong <xleong2@illinois.edu>
2018-12-04 22:48:45 -0600

    Use consts in format helper

--- frontend/src/utils/formatHelpers.js
+++ frontend/src/utils/formatHelpers.js
@@ -1,23 +1,31 @@
+// Human-readable text
+const middleDisplayText = 'Middle school';
+const intermediateDisplayText = 'Intermediate';
+
+// URL params
+const middleURLParam = 'middle';
+const intermediateURLParam = 'intermediate';
+
+// BE Storage/Return Values
+const middleDatabaseValue = 'Middle';
+const intermediateDatabaseValue = 'Intermediate';
+
 function URLParamToString(param) {
-  // param's possible values:
-  // - In our BE we store it as "Middle" or "Intermediate"
-  // - In our app router we route as "/middle" or "/intermediate"
-  if (param.toLowerCase && param.toLowerCase() === 'middle') {
-    return 'Middle School';
+  if (param === middleURLParam || param === middleDatabaseValue) {
+    return middleDisplayText;
   }
-  if (param.toLowerCase && param.toLowerCase() === 'intermediate') {
-    return 'Intermediate';
+  if (param === intermediateURLParam || param === intermediateDatabaseValue) {
+    return intermediateDisplayText;
   }
 }
 
 function URLParamToQueryParam(param) {
   // Desired return value should match BE
-  // - In our BE we store it as "Middle" or "Intermediate"
-  if (param.toLowerCase && param.toLowerCase() === 'middle') {
-    return 'Middle';
+  if (param === middleURLParam) {
+    return middleDatabaseValue;
   }
-  if (param.toLowerCase && param.toLowerCase() === 'intermediate') {
-    return 'Intermediate';
+  if (param === intermediateURLParam) {
+    return intermediateDatabaseValue;
   }
 }
 
