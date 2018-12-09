c6b3abe185c2594522d4a49917f5da759ad17047
Josh Byster <joshb1050@gmail.com>
2018-12-09 17:02:56 -0600

    Rebaed on master

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/bootswatch.css';
+import './styles/_bootswatch.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
