abbcf9d1175c6caf01d8a1379b7d957cd19b8093
Xuan Li Leong <xleong2@illinois.edu>
2018-12-09 12:27:29 -0600

    Make auth protection synchronous

--- frontend/src/pages/protection/AuthenticatedProtection.js
+++ frontend/src/pages/protection/AuthenticatedProtection.js
@@ -1,41 +1,24 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router';
-import { getUserData } from '../../utils/api';
+import Cookies from 'universal-cookie';
 
 // NOTE: This component only expects a SINGLE child in this.props.children.
 //       The child should be a complete page.
 
 class AuthenticatedProtection extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoggedIn: undefined
-    };
-    this.checkIsLoggedIn();
-  }
-
-  checkIsLoggedIn = async () => {
-    const { success, result, message } = await getUserData();
-    if (success) {
-      this.setState({ isLoggedIn: true });
-    } else {
-      this.setState({ isLoggedIn: false });
-    }
+  isLoggedIn = () => {
+    const cookies = new Cookies();
+    return cookies.get('jwt') !== undefined;
   };
 
   render() {
-    const { isLoggedIn } = this.state;
-    if (isLoggedIn === undefined) {
-      return null;
-    }
-
-    if (!isLoggedIn) {
+    if (this.isLoggedIn()) {
+      return (
+        <div>{React.cloneElement(this.props.children, { ...this.props })}</div>
+      );
+    } else {
       return <Redirect push to="/login" />;
     }
-
-    return (
-      <div>{React.cloneElement(this.props.children, { ...this.props })}</div>
-    );
   }
 }
 
