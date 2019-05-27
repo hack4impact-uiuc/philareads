6a7a50b7f2f1a361e10143021991b2fedf036514
ajdeleon <ajdeleon1064@gmail.com>
2019-05-26 20:45:35 -0400

    refactor component
    - make into functional component
    - use hooks for state management
    - fetch books for autocomplete component
    - replace regular input with autocomplete component

--- frontend/src/components/search/RedirectingSearchBar.js
+++ frontend/src/components/search/RedirectingSearchBar.js
@@ -1,43 +1,19 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import AutoComplete from '../search/AutoComplete';
+import { getAllPublishedBooks } from '../../utils/api';
 
-class RedirectingSearchBar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      query: ''
-    };
-  }
+const RedirectingSearchBar = () => {
+  const [bookTitles, setBookTitles] = useState([]);
 
-  handleChange = event => {
-    this.setState({ query: event.target.value });
-  };
-
-  handleKeyPress = event => {
-    if (event.key === 'Enter') {
-      event.preventDefault();
-      this.props.history.push(`/search?query=${this.state.query}`);
+  useEffect(() => {
+    async function fetchBooks() {
+      const response = await getAllPublishedBooks();
+      setBookTitles(response.result.results.map(book => book.name));
     }
-  };
+    fetchBooks();
+  }, []);
 
-  render() {
-    return (
-      <AutoComplete
-        suggestions={[
-          'cat in the hat',
-          'mouse on a motorcycle',
-          'dog in the house',
-          'Serafina and the Black Cloak'
-        ]}
-      />
-      // <Input
-      //   type="text"
-      //   placeholder="Search for a book..."
-      //   onChange={this.handleChange}
-      //   onKeyPress={this.handleKeyPress}
-      // />
-    );
-  }
-}
+  return <AutoComplete suggestions={bookTitles} />;
+};
 
 export default RedirectingSearchBar;
